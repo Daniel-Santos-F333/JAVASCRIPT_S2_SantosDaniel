6e7663b9b5859f48f645c3acb22200da090b99be
Daniel <cuentaanormal1208@gmail.com>
2025-07-18 11:26:01 -0500

    feat: :tada: beginning of a new big project

---
+++ Dia1/clase/scripts/script.js
@@ -0,0 +1,7 @@
+// ########################
+// ######   DIA 1 #########
+// ########################
+
+// ¡¡¡Primer comando!!!
+console.log("Hola Mundo! Pillen este número: ", 3+3+3+3+3+3)
+// Desarrollado por: Daniel Santos - C.C: 1.005.331.246
