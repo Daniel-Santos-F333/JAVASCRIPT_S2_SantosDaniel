680d0ef2987e583a7dab3cfd0ac553bcee16f89b
Daniel <cuentaanormal1208@gmail.com>
2025-07-18 13:57:55 -0500

    feat: :sparkles: introduce the function for  menuprincipal

---
+++ Dia1/tarea/scripts/script.js
@@ -0,0 +1,14 @@
+function mostrarMenuPrincipal() {
+    console.log("=============================================");
+    console.log("              Menu Principal                ");
+    console.log("=============================================");
+    console.log("Seleccione una opción:");
+    console.log("1. Registrar nuevo gasto");
+    console.log("2. Listar gastos");
+    console.log("3. Calcular total de gastos");
+    console.log("4. Generar reporte de gastos");
+    console.log("5. Salir");
+    console.log("=============================================");
+}
+
+mostrarMenuPrincipal()
